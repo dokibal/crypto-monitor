09ad7f58d9c24ab4bb7b1a9a60630780bcaad9f8
dokibal <doktor.balazs1@gmail.com>
2023-05-30 08:31:37 +0200

    Add home API call
    -Add home API call for testing purposes

--- src/main/java/bd/crypto/CryptoMonitorApplication.java
+++ src/main/java/bd/crypto/CryptoMonitorApplication.java
@@ -2,10 +2,19 @@ package bd.crypto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@Controller
+@SpringBootApplication
 public class CryptoMonitorApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CryptoMonitorApplication.class, args);
