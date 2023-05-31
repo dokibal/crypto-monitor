c8e6bed97fb9b5d4612dcbccf352d3a3aced2932
dokibal <doktor.balazs1@gmail.com>
2023-05-31 08:57:00 +0200

    Initialize heroku
    -Add Procfile
    -Edit pom.xml to be able to deploy heroku application

--- src/main/java/bd/crypto/CryptoMonitorApplication.java
+++ src/main/java/bd/crypto/CryptoMonitorApplication.java
@@ -2,12 +2,13 @@ package bd.crypto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class CryptoMonitorApplication {
 
 	@RequestMapping("/")
